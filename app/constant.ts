1df14cb07164280c5627ae58600d48c7d7a95e29
divyam234 <47589864+divyam234@users.noreply.github.com>
2024-07-22 21:02:43 +0530

    chore: add gpt-4-0125-preview

--- app/constant.ts
+++ app/constant.ts
@@ -195,6 +195,7 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "gpt-4o-mini": "2023-10",
   "gpt-4o-mini-2024-07-18": "2023-10",
   "gpt-4-vision-preview": "2023-04",
+  "gpt-4-0125-preview": "2023-12",
   // After improvements,
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
   "gemini-pro": "2023-12",
@@ -218,6 +219,7 @@ const openaiModels = [
   "gpt-4-vision-preview",
   "gpt-4-turbo-2024-04-09",
   "gpt-4-1106-preview",
+  "gpt-4-0125-preview"
 ];
 
 const googleModels = [
